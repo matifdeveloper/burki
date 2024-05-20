f8c34f0a57921dd310e28cbf548b8291c142c863
matifdeveloper <atifnoori.official@gmail.com>
2024-05-20 15:33:59 -0700

    Align extensions

--- lib/burki.dart
+++ lib/burki.dart
@@ -3,6 +3,7 @@ library burki;
 /// exports
 export 'src/extensions/padding_extensions.dart';
 export 'src/extensions/string_extensions.dart';
+export 'src/extensions/align_extensions.dart';
 export 'src/size_config/size_config.dart';
 export 'src/navigation/navigate.dart';
 export 'src/logger/logger.dart';
